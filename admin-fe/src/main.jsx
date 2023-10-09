ba27eccb563b0c13e8d5b5b4cacf7db0d5ee2831
cawfee <cawfee4@gmail.com>
2023-10-09 22:38:01 +0700

    fe push

--- admin-fe/src/main.jsx
+++ admin-fe/src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <React.StrictMode>
     <App />
-  </ChakraProvider>,
+  </React.StrictMode>,
 )
--- user-fe/src/App.jsx
+++ user-fe/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 
+
 function App() {
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
