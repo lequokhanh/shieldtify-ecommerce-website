0a4256ce7a6bf41bc6865417701dba6ed5d27b74
giakiet <g.kietle@gmail.com>
2023-11-19 22:56:03 +0700

    Add MarkdownRenderer component and test data

---
+++ user/src/components/SpecificationModal/index.jsx
@@ -0,0 +1,110 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Heading,
+    ListItem,
+    Text,
+    UnorderedList,
+    Box,
+    Button,
+} from '@chakra-ui/react'
+
+const SpecificationModal = ({ isOpen, onClose, specification }) => {
+    const specsJSON = JSON.parse(specification)
+
+    return (
+        <Modal blockScrollOnMount={'false'} isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent borderRadius={'10px'}>
+                <ModalHeader
+                    bgColor={'#2D2D2D'}
+                    color={'white'}
+                    borderTopRadius={'10px'}
+                >
+                    Specifications
+                </ModalHeader>
+                <ModalCloseButton
+                    bgColor={'white'}
+                    borderRadius={'50%'}
+                    size={'sm'}
+                    mt={'10px'}
+                    mr={'10px'}
+                />
+                <ModalBody>
+                    <UnorderedList>
+                        {Object.keys(specsJSON).map((specsKey) => {
+                            const spec = specsJSON[specsKey]
+                            return (
+                                <Box key={specsKey}>
+                                    <Heading
+                                        as="h4"
+                                        size="md"
+                                        mb={'10px'}
+                                        mt={'10px'}
+                                        fontWeight={'800'}
+                                        textDecoration={'underline'}
+                                    >
+                                        {specsKey}
+                                    </Heading>
+
+                                    <UnorderedList>
+                                        {Object.keys(spec).map(
+                                            (specDetailKey) => {
+                                                const specDetail =
+                                                    spec[specDetailKey]
+                                                return (
+                                                    <ListItem
+                                                        key={specDetailKey}
+                                                        ml="10px"
+                                                    >
+                                                        <Text
+                                                            fontSize={'20px'}
+                                                            fontWeight={'600'}
+                                                        >
+                                                            {specDetailKey
+                                                                .charAt(0)
+                                                                .toUpperCase() +
+                                                                specDetailKey
+                                                                    .slice(1)
+                                                                    .replace(
+                                                                        '_',
+                                                                        ' '
+                                                                    )}
+                                                            :{' '}
+                                                            <span className="font-normal">
+                                                                {specDetail}
+                                                            </span>
+                                                        </Text>
+                                                    </ListItem>
+                                                )
+                                            }
+                                        )}
+                                    </UnorderedList>
+                                </Box>
+                            )
+                        })}
+                    </UnorderedList>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button
+                        colorScheme="blackAlpha"
+                        bgColor="#2D2D2D"
+                        onClick={onClose}
+                        borderRadius="25px"
+                        w={'100%'}
+                    >
+                        Close
+                    </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default SpecificationModal
--- user/src/utils/routes.jsx
+++ user/src/utils/routes.jsx
@@ -1,61 +1,66 @@
-import Root from "../containers/Root"; 
-import SignUp from "../containers/SignUp";
-import RegisterComplete from "../containers/RegisterComplete";
-import SignIn from "../containers/SignIn";
-import ResetPassword from "../containers/ResetPassword";
-import NotFound from "../components/NotFound";
-import ResetPasswordComplete from "../containers/ResetPasswordComplete";
-import Home from "../containers/Home";
-import Product from "../containers/Products";
+import Root from '../containers/Root'
+import SignUp from '../containers/SignUp'
+import RegisterComplete from '../containers/RegisterComplete'
+import SignIn from '../containers/SignIn'
+import ResetPassword from '../containers/ResetPassword'
+import NotFound from '../components/NotFound'
+import ResetPasswordComplete from '../containers/ResetPasswordComplete'
+import Home from '../containers/Home'
+import Product from '../containers/Products'
+import ProductDetails from '../containers/ProductDetails'
 
 const routes = [
     {
-        path:  '/',
-        element: <Root/>,
+        path: '/',
+        element: <Root />,
         children: [
             {
                 path: '/home', // This makes it the default route for the root path
-                element: <Home/>,
-                name: "home",
+                element: <Home />,
+                name: 'home',
             },
-            {  
+            {
                 path: '/sign-up',
-                element: <SignUp/>,
-                name: "sign-up"
+                element: <SignUp />,
+                name: 'sign-up',
             },
             {
                 path: '/sign-in',
-                element: <SignIn/>,
-                name: "sign-in"
+                element: <SignIn />,
+                name: 'sign-in',
             },
             {
                 path: '/register-complete',
-                element: <RegisterComplete/>,
-                name: "register-complete",
+                element: <RegisterComplete />,
+                name: 'register-complete',
             },
             {
                 path: '/reset-password',
-                element: <ResetPassword/>,
-                name: "reset-password"
+                element: <ResetPassword />,
+                name: 'reset-password',
             },
             {
                 path: '/reset-password-complete',
-                element: <ResetPasswordComplete/>,
-                name: "reset-password-complete"
-            }
-            ,
+                element: <ResetPasswordComplete />,
+                name: 'reset-password-complete',
+            },
             {
                 path: '/products/',
-                element: <Product/>,
-                name: 'products'
+                element: <Product />,
+                name: 'products',
+            },
+            {
+                path: '/product/:id',
+                element: <ProductDetails />,
+                name: 'product',
             },
             {
                 path: '*',
-                element: <NotFound/>,
-                name: "not-found"
-            }
-        ]
+                element: <NotFound />,
+                name: 'not-found',
+            },
+        ],
     },
-];
+]
 
-export default routes;
+export default routes
