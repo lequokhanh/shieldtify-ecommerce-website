0b300e0e479236c5eb4e63a418ba48727b86322f
lequo <lequockhanhkt03@gmail.com>
2023-11-23 16:30:23 +0700

    Refactor promotion table columns

--- server_nodejs/migrations/20231122063208-change-column-name-in-promotion-table.js
+++ server_nodejs/migrations/20231122063208-change-column-name-in-promotion-table.js
@@ -13,11 +13,16 @@ module.exports = {
             type: Sequelize.FLOAT,
             allowNull: false,
         });
+        await queryInterface.addColumn('promotions', 'type', {
+            type: Sequelize.STRING,
+            allowNull: false,
+        });
     },
 
     async down(queryInterface, Sequelize) {
         await queryInterface.removeColumn('promotions', 'code');
         await queryInterface.removeColumn('promotions', 'max_discount');
+        await queryInterface.removeColumn('promotions', 'type');
         await queryInterface.addColumn('promotions', 'name', {
             type: Sequelize.STRING,
             allowNull: false,
