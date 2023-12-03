12b055b08f6ba1b8b08f7a902138bb21cf71a8e8
cawfee <cawfee4@gmail.com>
2023-12-03 16:39:42 +0700

    Revert "re-align checkout heading"
    This reverts commit a37c0708aec6a3a6811224521f41a0e7be0a0d3a.

--- user/src/containers/CheckOut/Card1.jsx
+++ user/src/containers/CheckOut/Card1.jsx
@@ -9,128 +9,120 @@ import {
     Td,
     Text,
     Th,
-    Tr,
-    Heading
+    Tr
 } from "@chakra-ui/react";
 import cart_icon from '../../assets/CheckOut/cart_icon.svg';
 
 const Card1 = ({items}) => {
     return (
-        <Flex flexDir="column">
-            <Box> 
-                <Heading color="#171A1F" fontSize="2.5rem" fontWeight="800" lineHeight="56px">
-                    Checkout
-                </Heading>
-            </Box>
-            <Box
-            padding="29px 15px"
-            borderRadius="18px"
-            boxShadow="0px 4px 20px rgba(0, 0, 0, 0.3)"
+        <Box
+        padding="29px 15px"
+        borderRadius="18px"
+        boxShadow="0px 4px 20px rgba(0, 0, 0, 0.3)"
+        >
+            <Flex
+            flexDir="column"
+            gap="30px"
             >
-                <Flex
-                flexDir="column"
-                gap="30px"
-                >
-                    <HStack pl="20px">
-                        <Image src={cart_icon} w="24px" h="24px" objectFit="contain"/>
-                        <Text
-                        color="shieldtify.checkout"
-                        fontSize="1.125rem"
-                        fontWeight="700"
-                        >
-                            Order Summary
-                        </Text>
-                    </HStack>      
-                        <TableContainer>
-                            <Table variant="unstyled">
-                                    <Tr >
-                                        <Th
-                                        color="#9095A1"
-                                        fontSize="0.875rem"
-                                        fontWeight="600"
-                                        >
-                                            Name
-                                        </Th>
-                                        <Th
-                                        color="#9095A1"
-                                        fontSize="0.875rem"
-                                        fontWeight="600"   
-                                        >
-                                            Price
-                                        </Th>
-                                        <Th
-                                        color="#9095A1"
-                                        fontSize="0.875rem"
-                                        fontWeight="600"                                
-                                        >
-                                            Quantity
-                                        </Th>
-                                        <Th
-                                        color="#9095A1"
-                                        fontSize="0.875rem"
-                                        fontWeight="600"                                
-                                        >
-                                            Total
-                                        </Th>
-                                    </Tr>
-                                    <Tbody
-                                    fontFamily="Inter, sans-serif"
+                <HStack pl="20px">
+                    <Image src={cart_icon} w="24px" h="24px" objectFit="contain"/>
+                    <Text
+                    color="shieldtify.checkout"
+                    fontSize="1.125rem"
+                    fontWeight="700"
+                    >
+                        Order Summary
+                    </Text>
+                </HStack>      
+                    <TableContainer>
+                        <Table variant="unstyled">
+                                <Tr >
+                                    <Th
+                                    color="#9095A1"
+                                    fontSize="0.875rem"
+                                    fontWeight="600"
                                     >
-                                        {
-                                            items.map((item) => {
-                                                return (
-                                                    <Tr key={item.itemid}>
-                                                        <Td>
-                                                            <Flex alignItems="center" gap="10px" w="400px" >
-                                                                <Image src={item.primary_img} w="60px" h="60px" objectFit="contain"/>
-                                                                <Text
-                                                                fontSize="0.875rem"
-                                                                fontWeight="700"
-                                                                color="shieldtify.checkout"
-                                                                isTruncated
-                                                                >
-                                                                    {item.name}
-                                                                </Text>
-                                                            </Flex>
-                                                        </Td>
-                                                        <Td >
-                                                            <Text
-                                                            fontSize="0.875rem"
-                                                            fontWeight="400"
-                                                            color="shieldtify.checkout"
-                                                            >
-                                                                {item.old_price ? item.old_price : item.price}$
-                                                            </Text>
-                                                        </Td>
-                                                        <Td textAlign="center">
-                                                            <Text
-                                                            fontSize="0.875rem"
-                                                            fontWeight="400"
-                                                            color="shieldtify.checkout"
-                                                            >
-                                                                {item.quantity}
-                                                            </Text>
-                                                        </Td>
-                                                        <Td>
+                                        Name
+                                    </Th>
+                                    <Th
+                                    color="#9095A1"
+                                    fontSize="0.875rem"
+                                    fontWeight="600"   
+                                    >
+                                        Price
+                                    </Th>
+                                    <Th
+                                    color="#9095A1"
+                                    fontSize="0.875rem"
+                                    fontWeight="600"                                
+                                    >
+                                        Quantity
+                                    </Th>
+                                    <Th
+                                    color="#9095A1"
+                                    fontSize="0.875rem"
+                                    fontWeight="600"                                
+                                    >
+                                        Total
+                                    </Th>
+                                </Tr>
+                                <Tbody
+                                fontFamily="Inter, sans-serif"
+                                >
+                                    {
+                                        items.map((item) => {
+                                            return (
+                                                <Tr key={item.itemid}>
+                                                    <Td>
+                                                        <Flex alignItems="center" gap="10px" w="400px" >
+                                                            <Image src={item.primary_img} w="60px" h="60px" objectFit="contain"/>
                                                             <Text
                                                             fontSize="0.875rem"
                                                             fontWeight="700"
                                                             color="shieldtify.checkout"
+                                                            isTruncated
                                                             >
-                                                                ${parseFloat((item.quantity * (item.old_price ? item.old_price : item.price)).toFixed(2))}
+                                                                {item.name}
                                                             </Text>
-                                                        </Td>
-                                                    </Tr>
-                                                )
-                                            })
-                                        }
-                                    </Tbody>
-                            </Table>
-                        </TableContainer>
-                    {/* </Flex> */}
-                </Flex>
-            </Box>
-        </Flex>
+                                                        </Flex>
+                                                    </Td>
+                                                    <Td >
+                                                        <Text
+                                                        fontSize="0.875rem"
+                                                        fontWeight="400"
+                                                        color="shieldtify.checkout"
+                                                        >
+                                                            {item.old_price ? item.old_price : item.price}$
+                                                        </Text>
+                                                    </Td>
+                                                    <Td textAlign="center">
+                                                        <Text
+                                                        fontSize="0.875rem"
+                                                        fontWeight="400"
+                                                        color="shieldtify.checkout"
+                                                        >
+                                                            {item.quantity}
+                                                        </Text>
+                                                    </Td>
+                                                    <Td>
+                                                        <Text
+                                                        fontSize="0.875rem"
+                                                        fontWeight="700"
+                                                        color="shieldtify.checkout"
+                                                        >
+                                                            ${parseFloat((item.quantity * (item.old_price ? item.old_price : item.price)).toFixed(2))}
+                                                        </Text>
+                                                    </Td>
+                                                </Tr>
+                                            )
+                                        })
+                                    }
+                                </Tbody>
+                        </Table>
+                    </TableContainer>
+                {/* </Flex> */}
+            </Flex>
+        </Box>
     )
 }
 
