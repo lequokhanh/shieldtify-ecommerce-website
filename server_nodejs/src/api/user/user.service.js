016d57b60e8c7aa852bd86c9ec00b6bc4b8846c6
lequo <lequockhanhkt03@gmail.com>
2023-12-06 09:30:41 +0700

    Remove sensitive client information

--- server_nodejs/src/api/user/user.service.js
+++ server_nodejs/src/api/user/user.service.js
@@ -150,6 +150,10 @@ module.exports = {
                 client.username = username;
             }
             await client.save();
+            delete client.password;
+            delete client.changed_password_at;
+            delete client.createdAt;
+            delete client.updatedAt;
             return {
                 statusCode: 200,
                 message: 'Update client successfully',
